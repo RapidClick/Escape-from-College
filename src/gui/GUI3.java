d1b147b956a9d7c6c597fa21f1e8d60220261f67
Connor Snee <snee0614@gmail.com>
2016-04-21 14:28:44 -0400

    added GUI3

---
+++ src/gui/GUI3.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+
+public class GUI3 {
+	
+	public GUI3() {
+		JFrame frame = new JFrame("Escape From College");
+		
+		JDesktopPane mainMenu = new JDesktopPane();
+		JInternalFrame saveScreen = new JInternalFrame();
+		
+		JDesktopPane playPane = new JDesktopPane();
+		JInternalFrame quickMenu = new JInternalFrame();
+	}
+
+}
