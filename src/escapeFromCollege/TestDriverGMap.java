d777b43e0614ff4702da8154dfd19cbc6ac54276
Connor Snee <snee0614@gmail.com>
2016-04-26 19:00:27 -0400

    fixed merge conflicts

--- src/escapeFromCollege/TestDriverGMap.java
+++ src/escapeFromCollege/TestDriverGMap.java
@@ -9,16 +9,9 @@ import java.io.FileNotFoundException;
 public class TestDriverGMap {
 	
 	public static void main(String[] args) throws FileNotFoundException{
-		File save = new File("Save1");
-<<<<<<< HEAD
 		GameMap2 g = new GameMap2();
-		//GUI gooey = new GUI(save, g);
-		g.play();
-=======
-		GameMap g = new GameMap();
 		GUI4 gooey = new GUI4();
 		//g.play();
->>>>>>> de39473478d01a1019a971eb5fba3804df5a210a
 		
 	}
 }
