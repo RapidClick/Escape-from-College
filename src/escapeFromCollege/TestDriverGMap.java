2cadef71218518f5fd50f2c5cf6687a445649a3d
Connor Snee <snee0614@gmail.com>
2016-04-26 20:13:35 -0400

    fixed use of wrong GUI

--- src/escapeFromCollege/TestDriverGMap.java
+++ src/escapeFromCollege/TestDriverGMap.java
@@ -1,15 +1,11 @@
 package escapeFromCollege;
-import gui.GUI;
-import gui.GUI3;
 import gui.GUI4;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 
 public class TestDriverGMap {
 	
 	public static void main(String[] args) throws FileNotFoundException{
-		File save = new File("Save1");
 		GameMap g = new GameMap();
 		GUI4 gooey = new GUI4();
 		//g.play();
